54a729b146b1f15bf64eec03adadf7b4d0143384
Ian Cardona <iancardona.dev@gmail.com>
2026-02-10 22:42:34 +0800

    more practice

--- simple_cat/simple_cat.go
+++ simple_cat/simple_cat.go
@@ -1,29 +1,30 @@
 package main
 
 import (
-	"io"
-	"log"
-	"os"
+	"context"
+	"database/sql"
 )
 
-func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("No file specified")
-	}
-	f, err := os.Open(os.Args[1])
+func DoSomeInserts(ctx context.Context, db *sql.DB, value1, value2 string) (err error) {
+	tx, err := db.BeginTx(ctx, nil)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-	defer f.Close()
-	data := make([]byte, 2048)
-	for {
-		count, err := f.Read(data)
-		os.Stdout.Write(data[:count])
-		if err != nil {
-			if err != io.EOF {
-				log.Fatal(err)
-			}
-			break
+	defer func() {
+		if err == nil {
+			err = tx.Commit()
 		}
+		if err != nil {
+			tx.Rollback()
+		}
+	}()
+	_, err = tx.ExecContext(ctx, "INSERT INTO FOO (val) values $1", value1)
+	if err != nil {
+		return err
 	}
+	return nil
+}
+
+func main() {
+	err := DoSomeInserts()
 }
