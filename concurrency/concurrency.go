8dafe711b2e0ecb0af96520415f9006195887ef1
Ian Cardona <iancardona.dev@gmail.com>
2026-02-17 23:50:08 +0800

    more concurrency

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,23 +1,102 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
 
-func main() {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
+type processor struct {
+	outA chan AOut
+	outB chan BOut
+	outC chan COut
+	inC  chan CIn
+	errs chan error
+}
 
+func GatherAndProcess(ctx context.Context, data Input) (COut, error) {
+	ctx, cancel := context.WithTimeout(ctx, 50*time.Millisecond)
+	defer cancel()
+	p := processor{
+		outA: make(chan AOut, 1),
+		outB: make(chan BOut, 1),
+		inC:  make(chan CIn, 1),
+		outC: make(chan COut, 1),
+		errs: make(chan error, 1),
+	}
+	p.launch(ctx, data)
+	inputC, err := p.waitForAB(ctx)
+	if err != nil {
+		return COut{}, err
+	}
+	p.inC <- inputC
+	out, err := p.waitForC(ctx)
+	return out, err
+}
+
+func (p *processor) launch(ctx context.Context, data Input) {
 	go func() {
-		v := 1
-		ch1 <- v
-		v2 := <-ch2
-		fmt.Println(v, v2)
+		aOut, err := getResultA(ctx, data.A)
+		if err != nil {
+			p.errs <- err
+			return
+		}
+		p.outA <- aOut
 	}()
 
-	v := 2
-	var v2 int
-	select {
-	case ch2 <- v:
-	case v2 = <-ch1:
+	go func() {
+		bOut, err := getResultB(ctx, data.B)
+		if err != nil {
+			p.errs <- err
+			return
+		}
+		p.outB <- bOut
+	}()
+
+	go func() {
+		select {
+		case <-ctx.Done():
+			return
+		case inputC := <-p.inC:
+			cOut, err := getResult(ctx, inputC)
+			if err != nil {
+				p.errs <- err
+				return
+			}
+			p.outC <- cOut
+		}
+	}()
+}
+
+func (p *processor) waitForAB(ctx context.Context) (CIn, error) {
+	var inputC CIn
+	count := 0
+	for count < 2 {
+		select {
+		case a := <-p.outA:
+			inputC.A = a
+			count++
+		case b := <-p.outB:
+			inputC.B = b
+			count++
+		case err := <-p.errs:
+			return CIn{}, err
+		case <-ctx.Done():
+			return CIn{}, ctx.Err()
+		}
 	}
-	fmt.Println(v, v2)
+
+	return inputC, nil
+}
+
+func (p *processor) waitForC(ctx context.Context) (COut, error) {
+	select {
+	case out := <-p.outC:
+		return out, nil
+	case err := <-p.errs:
+		return COut{}, err
+	case <-ctx.Done():
+		return COut{}, ctx.Err()
+	}
 }
