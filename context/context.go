ee84aaf1719234e7ea983a08f9ee521929c10c7d
Ian Cardona <iancardona.dev@gmail.com>
2026-02-21 23:45:02 +0800

    more practice

--- context/context.go
+++ context/context.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+)
+
+func logic(ctx context.Context, info string) (string, error) {
+	return "", nil
+}
 
 func main() {
 	fmt.Println("Hello, World!")
