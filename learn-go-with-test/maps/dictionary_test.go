31ffcdd2d1282fa06b60d1b4d6ad4b31f3252d56
Ian Cardona <iancardona.dev@gmail.com>
2026-03-02 23:18:16 +0800

    more practice - tests

--- learn-go-with-test/maps/dictionary_test.go
+++ learn-go-with-test/maps/dictionary_test.go
@@ -3,31 +3,14 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := Dictionary{"test": "this is just a test"}
+	dictionary := map[string]string{
+		"test": "this is just a test",
+	}
 
-	t.Run("known word", func(t *testing.T) {
-		got, _ := dictionary.Search("test")
-		want := "this is just a test"
-
-		assertStrings(t, got, want)
-	})
-
-	t.Run("unknown word", func(t *testing.T) {
-		_, err := dictionary.Search("unknown")
-		want := "could not find the word you were looking for"
-
-		if err == nil {
-			t.Fatal("expected to get an error.")
-		}
-
-		assertStrings(t, err.Error(), want)
-	})
-}
-
-func assertStrings(t testing.TB, got, want string) {
-	t.Helper()
+	got := Search(dictionary, "test")
+	want := "this is just a test"
 
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %q want %q given, %q", got, want, "test")
 	}
 }
