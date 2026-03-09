a37b13fe3d1deb48d8480dc6e3c073cfba32423b
Ian Cardona <iancardona.dev@gmail.com>
2026-03-09 12:00:00 +0800

    more practice with tests

--- learn-go-with-test/context/context_test.go
+++ learn-go-with-test/context/context_test.go
@@ -11,15 +11,21 @@ import (
 type SpyStore struct {
 	response  string
 	cancelled bool
+	t         *testing.T
 }
 
-func (s *SpyStore) Fetch() string {
-	time.Sleep(100 * time.Millisecond)
-	return s.response
+func (s *SpyStore) assertWasCancelled() {
+	s.t.Helper()
+	if !s.cancelled {
+		s.t.Error("store was not told to cancel")
+	}
 }
 
-func (s *SpyStore) Cancel() {
-	s.cancelled = true
+func (s *SpyStore) assertWasNotCancelled() {
+	s.t.Helper()
+	if s.cancelled {
+		s.t.Error("store was told to cancel")
+	}
 }
 
 func TestServer(t *testing.T) {
